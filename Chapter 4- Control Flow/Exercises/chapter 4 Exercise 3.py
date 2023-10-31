585989e4a8c74a57a8d701f3478b798f1ba81ec7
Zeppy0 <mdihossain104@gmail.com>
2023-10-31 09:30:18 +0400

    chapter 4 exercise 3.py

---
+++ Chapter 4- Control Flow/Exercises/chapter 4 Exercise 3.py
@@ -0,0 +1,26 @@
+#Version 1
+alien_color = 'green'
+if alien_color == 'green':
+    print("The player earned 5 points")
+elif alien_color =='grey':
+    print("The player earned 10 points")
+else:
+    print("The player earned 15 points")
+
+#Version 2
+alien_color = 'grey'
+if alien_color == 'green':
+    print("The player earned 5 points")
+elif alien_color == 'grey':
+    print("The player earned 10 points")
+else:
+    print("The player earned 15 points")
+
+#Version 3
+alien_color = 'red'
+if alien_color == 'green':
+    print("The player earned 5 points")
+elif alien_color == 'yellow':
+    print("The player earned 10 points")
+else:
+    print("The player earned 15 points")
